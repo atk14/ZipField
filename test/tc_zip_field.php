2c881adebc538786c6d441ee783057d2d79e1ea7
Jaromir Tomek <jaromir.tomek@gmail.com>
2019-11-06 15:26:03 +0100

    Tests refactored

--- test/tc_zip_field.php
+++ test/tc_zip_field.php
@@ -4,63 +4,80 @@ class TcZipField extends TcBase {
 	function test(){
 		$this->field = new ZipField([]);
 
-		// cz
-		$zip = $this->assertValid("123 45");
-		$this->assertEquals("123 45",$zip);
+		// Austria
+		$this->_test("AT",array(
+			"1010" => "1010"
+		),array("123 45"));
 
-		$zip = $this->assertValid(" 67890 ");
-		$this->assertEquals("67890",$zip);
+		// Czech Republic
+		$this->_test("CZ",array(
+				"123 45" => "123 45",
+				"67890" => "678 90",
+			),array("CW3 9SS")
+		);
 
-		$this->assertTrue($this->field->is_valid_for("CZ",$zip,$err));
-		$this->assertEquals("678 90",$zip); // filtering in progress: "67890" -> "678 90"
-		$this->assertEquals(null,$err);
+		// Slovakia
+		$this->_test("CZ",array(
+				"123 45" => "123 45",
+				"67890" => "678 90",
+			),array("CW3 9SS")
+		);
 
-		$this->assertFalse($this->field->is_valid_for("UK",$zip,$err));
-		$this->assertEquals(null,$zip);
-		$this->assertEquals(_("Please enter a valid zip code"),$err);
+		// Great Britain
+		$this->_test("GB",array(
+			"CW3 9SS" => "CW3 9SS",
+			"se50eg" => "SE50EG",
+		));
 
-		// IE, Ireland
-		$zip = $this->assertValid("V94 T2XR");
-		$this->assertValid("V94 T2XR",$zip);
-		$this->assertTrue($this->field->is_valid_for("IE",$zip,$err));
-		$this->assertValid("V94 T2XR",$zip);
-		//
-		$_zip = "D02AF30";
-		$zip = $this->assertValid($_zip);
-		$this->assertEquals("D02AF30",$zip);
-		$this->assertTrue($this->field->is_valid_for("IE",$zip,$err));
-		$this->assertEquals("D02 AF30",$zip);
+		// Ireland
+		$this->_test("IE",array(
+			"V94 T2XR" => "V94 T2XR",
+			"d02af30" => "D02 AF30",
+		),array("123 45"));
 
-		// gb
-		$zip = $this->assertValid("CW3 9SS");
-		$this->assertEquals("CW3 9SS",$zip);
-
-		$zip = $this->assertValid("se50eg");
-		$this->assertEquals("SE50EG",$zip);
-
-		// at
-		$zip = $this->assertValid("1010");
-		$this->assertEquals("1010",$zip);
-
-		// invalid
+		// Overall invalid codes
 		$this->assertInvalid("#");
 		$this->assertInvalid("1");
 
-		// accepting zip only for the one country only
+		// Accepting zip only for one country only (IE)
+		$this->field = new ZipField([
+			"country" => "IE",
+		]);
+		$zip = $this->assertValid("V94T2XR");
+		$this->assertEquals("V94 T2XR",$zip);
+		//
+		$err = $this->assertInvalid("123 33");
+		$this->assertEquals(_("Please enter a valid zip code"),$err);
+
+		// Accepting zip only for one country only (CZ)
 		$this->field = new ZipField([
 			"country" => "CZ",
 		]);
 		$zip = $this->assertValid("123 45");
 		$this->assertEquals("123 45",$zip);
-
+		//
 		$err = $this->assertInvalid("CW3 9SS");
 		$this->assertEquals(_("Enter the ZIP code as NNN NN"),$err);
-
+		//
 		$zip = $this->assertValid("56789");
 		$this->assertEquals("567 89",$zip);
 	}
 
-	function test_isValidFor(){
-		$field = new ZipField([]);
+	function _test($country,$valid_codes,$invalid_codes = array()){
+		foreach($valid_codes as $input => $expected){
+			$zip = $this->assertValid($input);
+			$this->assertTrue($this->field->is_valid_for($country,$zip,$err));
+			$this->assertEquals($expected,$zip);
+
+			$zip = $this->assertValid(" $input ");
+			$this->assertTrue($this->field->is_valid_for($country,$zip,$err));
+			$this->assertEquals($expected,$zip);
+		}
+
+		foreach($invalid_codes as $input){
+			$zip = $this->assertValid($input);
+			$this->assertFalse($this->field->is_valid_for($country,$zip,$err));
+		}
 	}
+
 }
